63a463e3de9c71829a06e92596c45f4d0e7816ef
LuluTchab <l.chaboudez@gmail.com>
2017-06-13 16:38:23 +0200

    v0.3 - handle more things

--- plugins/manage-plugin-config.php
+++ plugins/manage-plugin-config.php
@@ -16,6 +16,17 @@
       
    0.3
       - Now use a class to do things
+      - Add "unique" key information for option tables
+      - Add new option table (wp_postmeta)
+      - Add information about table relation (not real foreign keys but exists) to be able to map correctly 
+        the information we add between them (keep the mapping and avoid conflicts)
+      - Handle foreign values already existing in DB (not present in the configuration save file)
+      - Handle rows that are deleted and recreated with another ID (bigger) by the plugin. Duplicate entry 
+        error is handled and existing row id is recovered
+      
+   TODO :
+   - Add error check (existing files, etc...)
+   - 
 
    -------------------------------------------------------------------------------------------
 */
@@ -35,6 +46,8 @@
    5. Execute STEP 1 of this script
    6. Configure plugin AND stay on configuration page !
    7. Execute STEP 2 of this script to extract and save plugin configuration
+   8. Reinstall WordPress from scratch
+   9. Execute STEP 3 of this script to import plugin configuration.
 */
 
    
@@ -59,6 +72,7 @@
       var $plugin_name;
       var $db_link;
       var $config_tables;
+      var $tables_relations;
    
       
       /*
@@ -69,13 +83,22 @@
       function PluginConfig($plugin_name)
       {
          $this->plugin_name = $plugin_name;
-         $this->base_config = array();
-         /* Tables in which configuration is stored. Those tables must be sorted to satisfy foreign keys*/
-         $this->config_tables = array('wp_options'              => 'option_id',
-                                      'wp_terms'                => 'term_id',
-                                      'wp_termmeta'             => 'meta_id', // include wp_terms.term_id
-                                      'wp_term_taxonomy'        => 'term_taxonomy_id', // include wp_terms.term_id
-                                      'wp_term_relationships'   => 'object_id'); // include wp_term_taxonomy.term_taxonomy_id
+         $this->wp_tables   = array();
+         
+         /* Tables in which configuration is stored, with 'auto gen id' fields and 'unique field' (others than only auto-gen field). Those tables must be sorted to satisfy foreign keys*/
+         $this->config_tables = array('wp_postmeta'             => array('meta_id', null),
+                                      'wp_options'              => array('option_id', 'option_name'),
+                                      'wp_terms'                => array('term_id', null),
+                                      'wp_termmeta'             => array('meta_id', null), // include wp_terms.term_id
+                                      'wp_term_taxonomy'        => array('term_taxonomy_id', null), // include wp_terms.term_id
+                                      'wp_term_relationships'   => array(null, array('object_id', 'term_taxonomy_id'))); // include wp_term_taxonomy.term_taxonomy_id
+         
+         /* Relation between configuration tables. There are no explicit relation between tables in DB but there are relation coded in WP. */                             
+         $this->tables_relations = array('wp_termmeta'            => array('term_id' => 'wp_terms'),
+                                         'wp_term_taxonomy'       => array('term_id' => 'wp_terms'),
+                                         'wp_term_relationships'  => array('term_taxonomy_id' => 'wp_term_taxonomy'));
+                                         
+         
          
          /* Open DB connection */
          $this->db_link = mysqli_connect(WORDPRESS_HOST, MYSQL_ROOT_USER, MYSQL_ROOT_PASSWORD, WORDPRESS_DB);
@@ -107,6 +130,36 @@
          return sprintf(PLUGIN_CONFIG_FILE_FINAL, $this->plugin_name);
       }
       
+      /*
+         GOAL : Return information about foreign key information if exists
+         
+         $for_src_table    : Source table for which we have to check for a foreign key
+         $for_src_fiedl    : Field in the source table for which we have to check for a foreign key
+         
+         RET : NULL -> no foreign key
+               Target table name
+      */
+      protected function getForeignKeyTable($for_src_table, $for_src_field)
+      {
+
+         /* If there is foreign key information for the table, */
+         if(array_key_exists($for_src_table, $this->tables_relations))
+         {
+            /* If there is a foreign key defined for the field, */
+            if(array_key_exists($for_src_field, $this->tables_relations[$for_src_table]))
+            {
+               /* We return the exploded string <table>.<field> as result */
+               return $this->tables_relations[$for_src_table][$for_src_field];
+            }
+         
+         }/* END IF there is foreign key information for table */
+         
+         /* If we reach this point, it means no information was found. */
+         return null;
+      }
+      
+      
+      
       /* ----------------------------------------------------------------------- */
       
       /*
@@ -121,8 +174,13 @@
          $base_config = array();
          
          /* Going throught tables */
-         foreach($this->config_tables as $table_name => $id_field)
+         foreach($this->config_tables as $table_name => $fields_infos)
          {
+            /* Extract infos */
+            list($id_field, $unique_fields) = $fields_infos;
+      
+            /* if no $id field, we skip */
+            if($id_field === null) continue;
 
             $request = "SELECT MAX($id_field) AS 'max' FROM $table_name";  
             
@@ -166,11 +224,16 @@
          $base_config = unserialize(file_get_contents($base_config_file));         
          
          /* Going throught tables */
-         foreach($this->config_tables as $table_name => $id_field)
+         foreach($this->config_tables as $table_name => $fields_infos)
          {
+            /* Extract infos */
+            list($id_field, $unique_fields) = $fields_infos;
+            
             /* Get diff for table */
-            $request = "SELECT * FROM $table_name WHERE $id_field > $base_config[$table_name]";
-
+            $request = "SELECT * FROM $table_name";
+            /* If there's an ID field */
+            if($id_field!==null) $request.= " WHERE $id_field > $base_config[$table_name]";
+            
             if(($res = mysqli_query($this->db_link, $request))===false)
             {
                trigger_error(__FUNCTION__.": ".mysqli_error($this->db_link)."\n", E_USER_ERROR);
@@ -187,7 +250,7 @@
 
          }/* END LOOP Going through tables */   
          
-         print_r($config_diff);
+         //print_r($config_diff);
          
           $diff_config_file = $this->getPluginConfigFilename();
 
@@ -212,23 +275,117 @@
          $diff_config_file = $this->getPluginConfigFilename();
          $diff_config = unserialize(file_get_contents($diff_config_file));
          
+         /* To store ID mapping between configuration stored in files and what is inserted in DB */
+         $table_id_mapping = array();
+         
+         /* To tell if we execute this function in "simulation" mode (meaning that we create a transaction that we rollback right after)*/
+         $simulation=false;
+         
+         /* Start transaction if we are in "simulation" mode */
+         if($simulation)mysqli_autocommit($this->db_link, false);
+         
          /* Going throught tables */
-         foreach($this->config_tables as $table_name => $id_field)
+         foreach($this->config_tables as $table_name => $fields_infos)
          {
+         
+            /* Extract infos */
+            list($id_field, $unique_fields) = $fields_infos;
+
+            /* Creating mapping array for current table */
+            $table_id_mapping[$table_name] = array();
 
             /* Going through rows to add in table */
             foreach($diff_config[$table_name] as $row)
             {
+               $values = array();
+               /* Goint through fields/values in the row */
+               foreach($row as $field => $value)
+               {
 
-               $request = "INSERT IGNORE INTO $table_name (".implode(",",array_keys($row)).") VALUES('".implode("','",array_map('addslashes', $row))."')";
+                  /* If current field contains an "auto-generated id", */
+                  if($id_field==$field)
+                  {
+                     /* Empty value so it will be generated automatically */
+                     $values[] = '';
+                  }
+                  /* If we have information about foreign key, */
+                  else if(($target_table = $this->getForeignKeyTable($table_name, $field))!==null)
+                  { 
+                     /* If we have a mapping for the current value, */
+                     if(array_key_exists($value, $table_id_mapping[$target_table]))
+                     {
+                        /* Getting mapped id for current value */
+                        $values[] = $table_id_mapping[$target_table][$value];
+                        
+                     }
+                     else /* We don't have any mapping */
+                     {
+                        /* We take the value as it is because it is probably referencing something already existing in the DB 
+                           (and not present in the saved configuration for the plugin) */
+                        $values[] = $value;
+                        
+                     }
+                  }
+                  else /* We can take the value present in the config file (with 'addslashes' to be sure) */
+                  {
+                     $values[] = addslashes($value);
+                  }
+                  
+               }/* END LOOPING through fields/values in the row*/
+            
+            
+
+               $request = "INSERT IGNORE INTO $table_name VALUES('".implode("','", $values)."')";
 
                if(($res = mysqli_query($this->db_link, $request))===false)
                {
                   trigger_error(__FUNCTION__.": ".mysqli_error($this->db_link)."\n", E_USER_ERROR);
                }
+               
+               /* Getting ID of inserted value */
+               $insert_id = mysqli_insert_id($this->db_link);
+               
+               
+               
+               /* If row wasn't inserted because already exists, (so it means we must have an 'auto-gen' field) */
+               if($insert_id==0 && $id_field !== null)
+               {
+                  /* Array transform if needed */
+                  if(!is_array($unique_fields)) $unique_fields = array($unique_fields);
+                  
+                  $search_conditions = array();
+                  /* Going through unique fields */
+                  foreach($unique_fields as $unique_field_name)
+                  {
+                     $search_conditions[] = $unique_field_name."='".$row[$unique_field_name]."'";
+                  }
+                  
+                  /* Creating request to search existing row information */               
+                  $request = "SELECT * FROM $table_name WHERE ".implode(" AND ", $search_conditions);
+
+                  if(($res = mysqli_query($this->db_link, $request))===false)
+                  {
+                     trigger_error(__FUNCTION__.": ".mysqli_error($this->db_link)."\n", E_USER_ERROR);
+                  }
+                  
+                  $res = mysqli_fetch_assoc($res);
+                  /* Getting ID of existing row */
+                  $insert_id = $res[$id_field];
+                  
+               }/* END IF row wasn't inserted */
+               
+               
+               
+               /* Save ID mapping from data present in file TO row inserted in DB */
+               $table_id_mapping[$table_name][$row[$id_field]] = $insert_id;
+               
             } /* END LOOP Going through table rows */
          
          }/* END LOOP Going through tables */
+         
+         
+         /* If simulation, we rollback the transaction */
+         if($simulation)mysqli_rollback($this->db_link);
          
          $this->closeDBConnection();
       }
