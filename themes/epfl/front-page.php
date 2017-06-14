5e85f859ebcf71222745a402e6525617b028489f
Aline <aline.keller@gmail.com>
2017-06-14 17:23:40 +0200

    [themes/epfl] Improve page template markup

--- themes/epfl/front-page.php
+++ themes/epfl/front-page.php
@@ -15,46 +15,46 @@
 get_header(); ?>
 
 <div class="wrap">
-<div class="grid">
+  <div class="grid">
 
-<div id="primary" class="content-area col col-l-8">
-	<main id="main" class="site-main" role="main">
-
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
-
-		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param $num_sections integer
-			 */
-			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-			global $twentyseventeencounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$twentyseventeencounter = $i;
-				twentyseventeen_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+    <div id="primary" class="content-area col col-l-8">
+    	<main id="main" class="site-main" role="main">
+    
+    		<?php // Show the selected frontpage content.
+    		if ( have_posts() ) :
+    			while ( have_posts() ) : the_post();
+    				get_template_part( 'template-parts/page/content', 'front-page' );
+    			endwhile;
+    		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+    			get_template_part( 'template-parts/post/content', 'none' );
+    		endif; ?>
+    
+    		<?php
+    		// Get each of our panels and show the post data.
+    		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
+    
+    			/**
+    			 * Filter number of front page sections in Twenty Seventeen.
+    			 *
+    			 * @since Twenty Seventeen 1.0
+    			 *
+    			 * @param $num_sections integer
+    			 */
+    			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
+    			global $twentyseventeencounter;
+    
+    			// Create a setting and control for each of the sections available in the theme.
+    			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+    				$twentyseventeencounter = $i;
+    				twentyseventeen_front_page_section( null, $i );
+    			}
+    
+    	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
+    
+    	</main><!-- #main -->
+    </div><!-- #primary -->
+    
+    <?php get_sidebar(); ?>
 
   </div><!-- .grid -->
 </div><!-- .wrap -->
--- themes/epfl/page.php
+++ themes/epfl/page.php
@@ -17,27 +17,31 @@
 
 get_header(); ?>
 
-<div class="wrap">POUET
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="wrap">
+  <div class="grid">
+  	<div id="primary" class="content-area col col-l-8">
+  		<main id="main" class="site-main" role="main">
+  
+  			<?php
+  			while ( have_posts() ) : the_post();
+  
+  				get_template_part( 'template-parts/page/content', 'page' );
+  
+  				// If comments are open or we have at least one comment, load up the comment template.
+  				if ( comments_open() || get_comments_number() ) :
+  					comments_template();
+  				endif;
+  
+  			endwhile; // End of the loop.
+  			?>
+  
+  		</main><!-- #main -->
+  	</div><!-- #primary -->
+    
+    <?php get_sidebar(); ?>
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/page/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </div><!-- .grid -->
 </div><!-- .wrap -->
 
-<?php 
-  get_sidebar();
+<?php
   get_footer();
