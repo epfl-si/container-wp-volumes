cf25f2a530258fc2203d976a5ff0c7408849eb51
Aline <aline.keller@gmail.com>
2017-06-16 12:00:11 +0200

    [themes/epfl] Enqueue compiled css stylesheet

--- themes/epfl/functions.php
+++ themes/epfl/functions.php
@@ -57,7 +57,8 @@ function enqueue_theme_styles() {
 	wp_enqueue_style('grid', get_stylesheet_directory_uri() .'/assets/css/stylisticss.grid.css');
 	
 	// enqueue child styles
-	wp_enqueue_style( 'child-styles', get_stylesheet_uri() );
+	wp_enqueue_style('child-styles', get_stylesheet_uri() );
+	wp_enqueue_style('compiled-styles', get_stylesheet_directory_uri() .'/assets/css/style.css');
 	
 }
 add_action('wp_enqueue_scripts', 'enqueue_theme_styles', 10000000001 );
