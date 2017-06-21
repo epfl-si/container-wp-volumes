e505b52a6f924bbca4712f9163eecb8c43765bb6
Aline <aline.keller@gmail.com>
2017-06-21 17:26:41 +0200

    [themes/epfl + mu-plugins] Remove clear fix class form HTML and add it as a mixin in the stylesheet

--- mu-plugins/epfl-functions.php
+++ mu-plugins/epfl-functions.php
@@ -187,7 +187,7 @@ function faq_item( $atts, $content = null ) {
 add_shortcode('faq-item', 'faq_item');
 
 function colored_box( $atts, $content = null ) { 
-  $return .= '<section class="colored-box clearfix">';
+  $return .= '<section class="colored-box">';
   $return .= do_shortcode($content);
   $return .= '</section>';
   return $return;
