12211aad60a6a83a61be216fc9ca1e484738fbff
Luc Venries <luc.venries@epfl.ch>
2017-07-04 10:22:56 +0200

    Removed epfl.bicule.ch theme and add simple-sitemap plugin

--- wp-content/themes/epfl.bicube.ch/entry-footer.php
+++
@@ -1,7 +0,0 @@
-<div class="entry-footer">
-  <span class="cat-links"><?php _e( 'Categories: ', 'supersimple' ); ?><?php the_category( ', ' ); ?></span>
-  <span class="tag-links"><?php the_tags(); ?></span>
-  <?php if ( comments_open() ) {
-    echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'supersimple' ) ) . '</a></span>';
-  } ?>
-</div>
--- wp-content/themes/epfl.bicube.ch/loop-index.php
+++
@@ -1,8 +0,0 @@
-
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php get_template_part( 'entry' ); ?>
-    <?php comments_template(); ?>
-  <?php endwhile; endif; ?>
-  <?php get_template_part( 'nav', 'below' ); ?>
-
-
