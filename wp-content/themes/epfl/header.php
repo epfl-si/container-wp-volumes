986930ee3e0668921474a8f40bf58b342fd7637f
Aline <aline.keller@gmail.com>
2017-07-04 18:39:55 +0200

    [themes/epfl] Add EPFL banner header

--- wp-content/themes/epfl/header.php
+++ wp-content/themes/epfl/header.php
@@ -19,12 +19,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<?php readfile("http://www.epfl.ch/templates/fragments/header.sig.html")?>
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'epfl' ); ?></a>
+	
+	<div class="header-top wrap">
+  	<?php  readfile("http://www.epfl.ch/templates/fragments/header.fr.html"); ?>
+	</div><!-- .header-top -->
 
 	<header id="masthead" class="site-header" role="banner">
 
